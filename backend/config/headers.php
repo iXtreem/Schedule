d50bcb0166f94bcb7ca783ca608c0082b3b0277d
iXtreem <107632266+iXtreem@users.noreply.github.com>
2026-09-24 00:37:40 +0500

    Delete backend directory

--- backend/config/headers.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-  exit(0);
-}
