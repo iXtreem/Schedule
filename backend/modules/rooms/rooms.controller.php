49c63c11ddf7318727f49dd06a8750620e9acdb1
iXtreem <107632266+iXtreem@users.noreply.github.com>
2026-09-24 03:05:23 +0500

    Delete backend directory

--- backend/modules/rooms/rooms.controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../../lib/response.php';
-require_once __DIR__ . '/rooms.repo.php';
-
-function roomsController($conn, $method) {
-  if ($method !== 'GET') errorJson('Method not allowed', 405);
-  sendJson(repoGetRooms($conn));
-}
